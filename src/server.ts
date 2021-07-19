b3fdede418d8f0fa8700e4ca36e5194814c16f2d
Alef Castelo <alefaraujocastelo@gmail.com>
2021-07-19 06:53:19 -0500

    configuring prettier eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from 'express'
-import routes from './routes'
-import dotenv from 'dotenv'
+import express from 'express';
+import routes from './routes';
+import dotenv from 'dotenv';
 
-const app = express()
+dotenv.config({ path: './.env' });
 
-app.use(routes)
+const app = express();
 
-dotenv.config({ path: './.env' })
+app.use(routes);
 
 app.listen(process.env.HOST_PORT, () => {
-    console.log(`Server running on server http://localhost:${process.env.HOST_PORT}`)
-})
+  console.log(`Server running: http://localhost:${process.env.HOST_PORT}`);
+});
